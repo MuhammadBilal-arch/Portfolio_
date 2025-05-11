3c47ef7a3adee8b85df2fbd83762dec2e0411a70
Muhammad Bilal <expbilal@gmail.com>
2025-05-12 04:00:05 +0500

    Added: seo

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export const metadata = {
     siteName: "Muhammad Bilal Portfolio",
     images: [
       {
-        url: "https://mb-blue.vercel.app/og-image.png",
+        url: "https://mb-blue.vercel.app/og-image.jpeg",
         width: 1200,
         height: 630,
         alt: "Muhammad Bilal Portfolio Preview",
